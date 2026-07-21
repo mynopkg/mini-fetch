025ae27de8c45487c085f635b6196611585f79b2
mynolog <minholee.log@gmail.com>
2026-07-21 22:35:55 +0900

    refactor: add MiniFetchMethod type and restructure MiniFetchRequest/Options hierarchy

--- src/types/MiniFetch.ts
+++ src/types/MiniFetch.ts
@@ -1,8 +1,17 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-export interface MiniFetchOptions<T = any> extends Omit<RequestInit, 'body'> {
-  body?: T
+export type MiniFetchMethod = 'GET' | 'POST' | 'PATCH' | 'PUT' | 'DELETE'
+
+export interface MiniFetchOptions extends MiniFetchRequest {
   autoParseJson?: boolean
   timeout?: number
+}
+
+export interface MiniFetchRequest<T = any> extends Omit<RequestInit, 'body'> {
+  body?: T
+}
+
+export interface MiniFetchResponse<T = any> extends Response {
+  data?: T
 }
 
 export interface MiniFetchApi {
@@ -11,13 +20,4 @@ export interface MiniFetchApi {
   patch: <T = any>(url: string, options?: MiniFetchOptions) => Promise<MiniFetchResponse<T>>
   put: <T = any>(url: string, options?: MiniFetchOptions) => Promise<MiniFetchResponse<T>>
   delete: <T = any>(url: string, options?: MiniFetchOptions) => Promise<MiniFetchResponse<T>>
-}
-
-export interface MiniFetchRequest extends RequestInit {
-  autoParseJson?: boolean
-  timeout?: number
-}
-
-export interface MiniFetchResponse<T = any> extends Response {
-  data?: T
 }
