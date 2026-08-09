20f4b31aa6533a1d9b5d982b647f19687e28968e
mynolog <minholee.log@gmail.com>
2026-08-09 16:18:52 +0900

    feat(errors): export UnsupportedRuntimeError

--- src/errors/index.ts
+++ src/errors/index.ts
@@ -1,3 +1,4 @@
 export { RequestError } from './RequestError'
 export { HttpError } from './HttpError'
 export { TimeoutError } from './TimeoutError'
+export { UnsupportedRuntimeError } from './UnsupportedRuntimeError'
