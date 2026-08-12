7753a70889fe4636b9735cbfb7881d8521995bc8
mynolog <minholee.log@gmail.com>
2026-08-12 16:52:42 +0900

    chore: rename helpers.ts to api.ts for better clarity

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { miniFetch } from './core/miniFetch'
 export { createMiniFetch } from './core/factories'
-export { miniFetchApi } from './core/helpers'
+export { miniFetchApi } from './core/api'
 export { HttpError, TimeoutError, RequestError } from './errors'
